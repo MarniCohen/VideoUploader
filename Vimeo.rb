1831003ced47eacc85cfa31c41cdf3de75b852e4
MarniCohen <marni.a.cohen@gmail.com>
2014-05-01 14:07:33 -0700

    try again

--- Vimeo.rb
+++ Vimeo.rb
@@ -129,11 +129,11 @@ end
 
 #Finds and uploads file
 def uploader()
-  puts "Make sure the file you want to upload is in ~/Desktop/Video Uploader/Videos/"
+  puts "Make sure the file you want to upload is in Video Uploader/Videos/"
 
   filename = pickfile()
 
-  file = "Videos/" + filename
+  file = "/Videos/" + filename
   puts "Ok, wait a minute while I do some magic."
   upload = Vimeo::Advanced::Upload.new(@consumerKey, @consumerSecret, :token => @token, :secret => @tokenSecret)
 
